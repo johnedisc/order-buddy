39045f1532595b875eae2447b17e11b0f9e48cfb
johnedisc <christopher.johnedis@gmail.com>
2023-03-03 16:20:53 -0800

    Test 2. Positive fail. Check for Title parameter. Constructor doesnt take a parameter

--- OrderBuddy.Tests/ModelTests/OrderTests.cs
+++ OrderBuddy.Tests/ModelTests/OrderTests.cs
@@ -14,11 +14,25 @@ namespace OrderBuddy.Tests
 
     }
 
+    //Arrange
+    //Title, Description, Price, Date
+    List<string> sampleParameters = new List<string> { "flour","50 lbs bag","$25.25","2/23/23" };
+
     [TestMethod]
-    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    public void OrderConstruct_MakesInstanceOfOrder_Order()
     {
-      Order newOrder = new Order("test");
-      Assert.AreEqual(typeof(Order), newOrder.GetType());
+      Order testOrder = new Order(sampleParameters[0],sampleParameters[1]);
+      Assert.AreEqual(typeof(Order), testOrder.GetType());
+    }
+
+    [TestMethod]
+    public void OrderConstruct_ReturnsInputTitle_Order()
+    {
+      //Act
+      Order testOrder = new Order(sampleParameters[0],sampleParameters[1]);
+
+      //Assert
+      Assert.AreEqual(sampleParameters[0], testOrder.Title);
     }
 
   }
