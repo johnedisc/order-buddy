a384aa5c9621a4ad04fc40b3f40a4e8d8775b92c
johnedisc <christopher.johnedis@gmail.com>
2023-03-07 12:47:45 -0800

    creating detail view for vendors

--- OrderBuddy/Controllers/OrdersController.cs
+++ OrderBuddy/Controllers/OrdersController.cs
@@ -13,5 +13,18 @@ namespace OrderBuddy.Controllers
         return View(passedVendor);
       }
 
+      [HttpPost("/vendors/{id}/orders/create")]
+      public ActionResult Create(string orderTitle,string orderDescription, string orderPrice,string orderDate)
+      {
+        Order newVendorOrder = new Order(string orderTitle,string orderDescription, string orderPrice,string orderDate);
+        Vendor passedVendor = Vendor.Find(id);
+        return ResultToAction(passedVendor);
+      }
+      [HttpPost("/vendors")]
+      public ActionResult Create(string vendorName, string vendorDescription)
+      {
+        Vendor newVendor = new Vendor(vendorName, vendorDescription);
+        return RedirectToAction("Index");
+      }
     }
 }
